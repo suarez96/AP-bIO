01a150496dc3539d59ae8f7d01e76dc2af6ac11d
suarez96 <augus@augustos-mbp.home>
2024-06-05 13:58:26 -0400

    dataloader updated to work with yaml

--- Dataloader.py
+++ Dataloader.py
@@ -2,11 +2,13 @@ from Data import MarshData, FantasiaData
 from Transforms import WPC
 import Transforms
 from Signal import Signal
-from train_utils
+from train_utils import get_rolling_windows
 from tsai.all import get_splits, TSRegression, TSStandardize, get_ts_dls
+import os
+import numpy as np
 
-
-def build_windows(
+def build_ECG_input_windows(
+        args: dict,
         dataset: list
     ):
     """
@@ -20,23 +22,19 @@ def build_windows(
     X_ecg_rolling_train_stack = []
     y_ip_train_stack = []
 
+    # yaml args define the params of each transform
+    global_ecg_pipeline = Transforms.build_transforms(
+        args['yaml_args']['global_ecg_pipeline']
+    )
+    global_ip_pipeline = Transforms.build_transforms(
+        args['yaml_args']['global_ip_pipeline']
+    )
+
     # take global ECG and break it into parts after applying transform
     for subject in dataset:
 
-        input_ecg_raw = subject.ECG().transform(transforms=[
-            # should take transforms from args, i.e. from params.yml
-            Transforms.Crop(start_time_XGB, end_time_XGB), # C
-            Transforms.MinMaxScale(), # S
-            lambda x: x-x.mean(), # M
-            lambda x: detrend(x), # D
-        ])
-
-        input_ip_raw = subject.IP().transform(transforms=[
-            Transforms.Crop(start_time_XGB, end_time_XGB),
-            Transforms.MinMaxScale(),
-            lambda x: x-x.mean(),
-            lambda x: detrend(x),
-        ])
+        input_ecg_raw = subject.ECG().transform(transforms=global_ecg_pipeline)
+        input_ip_raw = subject.IP().transform(transforms=global_ip_pipeline)
 
         # extract transformed data
         input_ecg = input_ecg_raw.transformed_data
@@ -45,29 +43,59 @@ def build_windows(
         # get rolling windows
         X_ecg_rolling = get_rolling_windows(
             input_ecg, 
-            window_size=args['hparams']['seq_len'], 
-            jump=args['hparams']['train_jump_size']
+            window_size=args['yaml_args']['hparams']['seq_len'], 
+            jump=args['yaml_args']['hparams']['jump_size']
         )
-        y_ip = input_ip.data[window_size-1:][::train_jump_size]
+        y_ip = input_ip.data[args['yaml_args']['hparams']['seq_len']-1:][::args['yaml_args']['hparams']['jump_size']]
 
         X_ecg_rolling_train_stack.append(X_ecg_rolling)
         y_ip_train_stack.append(y_ip)
 
     return np.vstack(X_ecg_rolling_train_stack), np.stack(y_ip_train_stack).flatten()
 
-def build_loader(args):
 
-    # build_loader ()
-    dataset = [
-        MarshData(f"../MARSH/{i}/") for i in os.listdir("../MARSH/") if len(i) == 4
-    ][:4] # TODO: REMOVE INDEXING WHEN DONE
+def loader_from_dataset(
+    args,
+    dataset,
+    batch_tfms = TSStandardize(by_sample=True), # Standardize data TODO add more batch transforms
+    valid_size = 0.2,
+    shuffle=True
+):
 
-    X_ecg_rolling_train_stack_np, y_ip_train_stack_np = build_windows(dataset)
-    tsai_X_train = X_ecg_rolling_train_stack_np.reshape(-1, 1, args['hparams']['seq_len'])
+    """
+    Construct the dataloader based on a train or test list of MarshData/FantasiaData objects.
+    shuffle: shuffle data in the loader
+    """
+    X_ecg_rolling_train_stack_np, y_ip_train_stack_np = build_ECG_input_windows(args=args, dataset=dataset)
+    tsai_X_train = X_ecg_rolling_train_stack_np.reshape(-1, 1, args['yaml_args']['hparams']['seq_len'])
     tsai_y_train = y_ip_train_stack_np.reshape(-1, 1)
-    splits = get_splits(tsai_y_train, valid_size=0.2, stratify=True, random_state=23, shuffle=True)
+    splits = get_splits(tsai_y_train, valid_size=valid_size, stratify=True, random_state=23, shuffle=shuffle, show_plot=False)
+    # TODO investigate this step
     tfms  = [None, TSRegression()]
-    # TODO add more batch transforms
-    batch_tfms = TSStandardize(by_sample=True)  # Standardize data
-    dls = get_ts_dls(tsai_X_train, tsai_y_train, splits=splits, tfms=tfms, batch_tfms=batch_tfms, bs=batch_size)
+    dls = get_ts_dls(tsai_X_train, tsai_y_train, splits=splits, tfms=tfms, batch_tfms=batch_tfms, bs=args['yaml_args']['hparams']['batch_size'])
     return dls
+
+def build_loaders(args, test=False, shuffle_test=False):
+
+    """
+    test: whether to build a test dataloader or return None
+    shuffle_test (bool): whether to shuffle the data in the test loader. usuallly false because we want to build the predictions
+    in time order
+    """
+
+    dataset = [
+        MarshData(os.path.join(args['marsh_path'], i)) for i in os.listdir(args['marsh_path']) if len(i) == 4
+    ]
+
+    # TODO add shuffle or tracking for subjects by ID
+    train_dataset = dataset[:int(args['yaml_args']['data']['train_samples'])]
+    train_dataloader = loader_from_dataset(args=args, dataset=train_dataset)
+    
+    if test:
+        test_dataset = dataset[int(args['yaml_args']['data']['train_samples']):]
+        test_dataloader = loader_from_dataset(args=args, dataset=test_dataset, valid_size=0, shuffle=shuffle_test)
+    else:
+        test_dataloader = None
+
+    return train_dataloader, test_dataloader
+
