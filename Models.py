809bf1f74cb42af71ac60a52765095dbb3b45e00
sjy-chen <samueljychen@gmail.com>
2024-06-07 14:06:13 -0400

    eval log

--- Models.py
+++ Models.py
@@ -1,4 +1,4 @@
-from tsai.all import MSELossFlat, rmse, TST, Learner, ShowGraph
+from tsai.all import MSELossFlat, rmse, TST, Learner, ShowGraph, load_learner
 from abc import ABC, abstractmethod
 import numpy as np
 import os
@@ -7,8 +7,12 @@ logger = logging.getLogger(__name__)
 
 class Model(ABC):
 
-    def __init__(self, export_dir_root='models', **kwargs):
-        self.run_id = hex(np.random.randint(0, 8**8))
+    def __init__(self, export_dir_root='models', path=None, **kwargs):
+        if path is not None:
+            self.path = path
+            self.run_id = path.split("/")[-1].replace(".pkl", "")
+        else:
+            self.run_id = hex(np.random.randint(0, 8**8))
         self.framework = "no_framework"
         self.export_dir_root = export_dir_root
 
@@ -32,7 +36,10 @@ class TSAITransformer(Model):
         self.framework = 'tsai'
         self.model = TST(dataloader.vars, dataloader.c, seq_len=seq_len)
         # the fastai object that manages the training loop
-        self.learner = Learner(dataloader, self.model, loss_func=MSELossFlat(), metrics=rmse)
+        if self.path is None:
+            self.learner = Learner(dataloader, self.model, loss_func=MSELossFlat(), metrics=rmse)
+        else:
+            self.learner = load_learner(self.path)
 
     def train(self, iters, lr):
         logger.info("Training finished")
