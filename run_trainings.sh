bf79aad19d46ed81963bd738ac1be8939e20d611
suarez96 <augus@augustos-mbp.home>
2024-07-28 13:02:12 -0400

    Niagara params upload

---
+++ run_trainings.sh
@@ -0,0 +1,16 @@
+
+python main.py
+python main.py
+python main.py
+python main.py
+python main.py
+python main.py -y params_1.yml
+python main.py -y params_1.yml
+python main.py -y params_1.yml
+python main.py -y params_1.yml
+python main.py -y params_1.yml
+python main.py -y params_2.yml
+python main.py -y params_2.yml
+python main.py -y params_2.yml
+python main.py -y params_2.yml
+python main.py -y params_2.yml
